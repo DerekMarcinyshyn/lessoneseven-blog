97faa2b9fbf1be6ce8461a6784e18a48672657ea
Derek Marcinyshyn <derek@monasheemountainmultimedia.com>
2014-09-08 14:15:03 -0700

    add style to header

--- templates/head.php
+++ templates/head.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 
   <?php wp_head(); ?>
 
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,12 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<div class="container-desktop-top container hidden-xs">
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php echo get_bloginfo('url') ?>/assets/img/less-logo_208x100.jpg" alt="LESS 17" width="208" height="100" />
+    </a>
+
+    <p class="pull-right phone-number"><a href="tel:1-403-228-9199">1.403.228.9199</a></p>
+</div>
+
+<header class="banner navbar navbar-default" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -7,7 +15,9 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand visible-xs-block" href="<?php echo esc_url(home_url('/')); ?>">
+          <img src="<?php echo get_bloginfo('url') ?>/assets/img/less-logo_208x100.jpg" alt="LESS 17" width="80"/>
+      </a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
